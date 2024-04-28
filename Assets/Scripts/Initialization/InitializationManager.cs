bac06ee0f3df27524b4876c2c95fd293783353c3
Serkan <yilserkan66@gmail.com>
2024-04-28 11:32:05 +0300

    feat : Initialize and sign in to ugs at the start of the game

--- Assets/Scripts/Initialization/InitializationManager.cs
+++ Assets/Scripts/Initialization/InitializationManager.cs
@@ -2,12 +2,17 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using CardGame.Extensions;
 using CardGame.Inventory;
 using CardGame.SceneManagement;
 using CardGame.ServiceManagement;
 using CardGame.Singleton;
 using CardGame.Utils;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Core.Environments;
+using Unity.Services.RemoteConfig;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.SceneManagement;
@@ -17,6 +22,14 @@ namespace CardGame.Initialization
 {
     public class InitializationManager : MonoBehaviour
     {
+        public struct userAttributes {}
+        public struct appAttributes {}
+        
+        private readonly Dictionary<Environments, string> _environments = new Dictionary<Environments, string>()
+        {
+            { Environments.Production, "production" }
+        };
+        
         private void Start()
         {
             Initialize();
@@ -24,18 +37,55 @@ namespace CardGame.Initialization
 
         private async void Initialize()
         {
+            // initialize Unity's authentication and core services, however check for internet connection
+            // in order to fail gracefully without throwing exception if connection does not exist
+            if (Utilities.CheckForInternetConnection())
+            {
+                await InitializeRemoteConfigAsync();
+            }
+
+            RemoteConfigService.Instance.FetchCompleted += ApplyRemoteSettings;
+            RemoteConfigService.Instance.FetchConfigs(new userAttributes(), new appAttributes());
+        }
+        
+        async Task InitializeRemoteConfigAsync()
+        {
+            var options = new InitializationOptions().SetEnvironmentName(_environments[Environments.Production]);
+        
+            // initialize handlers for unity game services
+            await UnityServices.InitializeAsync(options);
+            
+            // remote config requires authentication for managing environment information
+            if (!AuthenticationService.Instance.IsSignedIn)
+            {
+                await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            }
+        }
+
+        async void ApplyRemoteSettings(ConfigResponse configResponse)
+        {
+            Debug.Log("RemoteConfigService.Instance.appConfig fetched: " + RemoteConfigService.Instance.appConfig.config);
+            Debug.Log("PlayerId : " + AuthenticationService.Instance.PlayerId);
+            // RemoteConfigService.Instance.GetConfig("Test");
+            // RemoteConfigService.Instance.appConfig.GetFloat("TEST");
+            
             ServiceLocator.LazyGlobal
                 .Get(out SceneLoader sceneLoader)
                 .Get(out ScriptableManagerInitializerMono scriptableManagerInitializer);
-
+            
             if (scriptableManagerInitializer != null)
             {
                 await scriptableManagerInitializer.InitializeScriptableManagers();
             }
-       
-            sceneLoader.OrNull()?.LoadScene(SceneType.GameScene);
             
-            PlayerInventory.Initialize();
+            await PlayerInventory.Initialize();
+                
+            sceneLoader.OrNull()?.LoadScene(SceneType.GameScene);
+        }
+        
+        public enum Environments
+        {
+            Production
         }
     }
 }
