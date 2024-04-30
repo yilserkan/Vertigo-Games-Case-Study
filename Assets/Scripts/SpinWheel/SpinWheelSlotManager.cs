2fe23cd065a6344c3aa768fdf263d5f493325651
Serkan <yilserkan66@gmail.com>
2024-04-30 18:59:10 +0300

    feat : spin wheel reward bg is now colored according to item rarity

--- Assets/Scripts/SpinWheel/SpinWheelSlotManager.cs
+++ Assets/Scripts/SpinWheel/SpinWheelSlotManager.cs
@@ -8,13 +8,33 @@ namespace CardGame.SpinWheel
 {
     public class SpinWheelSlotManager : MonoBehaviour
     {
+        [SerializeField] private Image _slotBgImage;
         [SerializeField] private Image _slotIcon;
         [SerializeField] private TextMeshProUGUI _amountText;
-
+        [SerializeField] private Color[] _bgColors;
+        [SerializeField] private Color _bombColor;
+        
         public void InitializeSlot(ItemData data, int amount)
         {
             _slotIcon.sprite = data.Sprite;
             _amountText.text = amount.ToString();
+            _slotBgImage.color = GetBgColor(data.Rarity, data.Type);
+        }
+
+        private Color GetBgColor(ItemRarity rarity, ItemType itemType)
+        {
+            if (itemType == ItemType.Bomb)
+            {
+                return _bombColor;
+            }
+            
+            var rarityIndex = (int)rarity;
+            if (_bgColors.Length > rarityIndex)
+            {
+                return _bgColors[rarityIndex];
+            }
+            
+            return Color.white;
         }
     }
 }
